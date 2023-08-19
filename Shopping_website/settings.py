a90606a4a53c1580324fa1b535aaac6f2222653e
yochaiankava <meshek.ankava@gmail.com>
2023-08-19 17:03:33 +0300

    modify settings.py

--- Shopping_website/settings.py
+++ Shopping_website/settings.py
@@ -33,7 +33,7 @@ if not DEBUG:
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'meshek-ankave-basic-ecommerce.onrender.com']
 
 
 
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
